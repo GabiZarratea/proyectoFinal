0eeb75c604aad255d1fa4fa7bc8cd14602609c25
Carlos Cantillo <133810602+CarlosCantillo25@users.noreply.github.com>
2023-06-20 21:26:32 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,116 @@
+export { url, imprimirTarjetas, crearPlantillaCarrito, imprimirTarjetasCarrito, imprimirDatosCarrito, };
+
+const url = "https://mindhub-xj03.onrender.com/api/petshop";
+
+const crearPlantilla= (producto) => {
+
+const articulosSeleccionados = JSON.parse(localStorage.getItem("seleccionados"))
+  
+  let href = location.pathname.includes("index")
+    ? "./assets/pages/details.html"
+    : "./details.html"
+
+  return `<div class="card" style="width: 20rem; text-align: center;" >
+                      <img src="${producto.imagen}" class="img_cards rounded-top-2">
+                      <div class="card-body">
+                          <h5 class="card-title">${producto.producto}</h5>
+                          <p>Price: $ ${producto.precio}</p>      
+                      </div>
+                      <div class="card-body d-flex justify-content-around">
+                          <button data-id="${producto._id}" class="btn btn-carrito btn-success ${articulosSeleccionados ? 'btn-danger' : 'btn-success'}"></button>
+                          <a href="${href} " class="btn btn-details"> Ver más </a>
+                      </div>
+                      
+                  </div>`;
+};
+
+function imprimirTarjetas(productos, container) {
+  imprimirDatos(productos, container);
+
+  function filterCards() {
+    const inputValue = searchInput.value.toLowerCase().trim();
+
+    const filteredCards = productos.filter(function (prod) {
+      return (
+        (inputValue === "" || prod.producto.toLowerCase().includes(inputValue))
+      );
+    });
+
+    container.innerHTML = "";
+    imprimirDatos(filteredCards, container);
+  }
+
+  searchButton.addEventListener("click", filterCards);
+}
+
+function imprimirDatos(array, lugar) {
+  let template = "";
+
+  for (let event of array) {
+    template += crearPlantilla(event);
+  }
+
+  lugar.innerHTML += template;
+}
+
+//-------------funciones para carrito------------/////
+
+const crearPlantillaCarrito = (producto) => {
+
+  const articulosSeleccionados = JSON.parse(localStorage.getItem("seleccionados"))
+    
+    let href = location.pathname.includes("index")
+      ? "./assets/pages/details.html"
+      : "./details.html"
+  
+    return `<div class="card" style="width: 20rem; text-align: center;" >
+                        <img src="${producto.imagen}" class="img_cards rounded-top-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${producto.producto}</h5>
+                            <p>Price: $ ${producto.precio}</p>      
+                        </div>
+                        <div class="card-body d-flex justify-content-around">
+                            <button data-id="${producto._id}" class="btn btn-carrito btn-success ${articulosSeleccionados ? 'btn-danger' : 'btn-success'}"></button>
+                            <a href="${href} " class="btn btn-details"> Ver más </a>
+                        </div>
+                                                
+                        <div>
+                        <button id="menos">-</button>
+                        <span id="text">1</span>
+                        <button id="mas">+</button>
+                        </div>   
+                        
+              </div>`;
+  };
+function imprimirTarjetasCarrito(productos, container) {
+      imprimirDatosCarrito(productos, container);
+    
+      function filterCards() {
+        const inputValue = searchInput.value.toLowerCase().trim();
+    
+        const filteredCards = productos.filter(function (prod) {
+          return (
+            (inputValue === "" || prod.producto.toLowerCase().includes(inputValue))
+          );
+        });
+    
+        container.innerHTML = "";
+        imprimirDatosCarrito(filteredCards, container);
+      }
+    
+      searchButton.addEventListener("click", filterCards);
+}
+
+function imprimirDatosCarrito(array, lugar) {
+  let template = "";
+
+  for (let event of array) {
+    template += crearPlantillaCarrito(event);
+  }
+
+  lugar.innerHTML += template;
+}
+
+
+
+
--- tienda.js
+++ tienda.js
@@ -1,38 +1,82 @@
 
-import {  url ,imprimirTarjetas } from "../JS/functions.js";
+import {  url, imprimirTarjetasCarrito, imprimirDatosCarrito, crearPlantillaCarrito } from "../JS/functions.js";
 
 const container = document.getElementById("container")
 let productos=[];
 let articulosSeleccionados = JSON.parse(localStorage.getItem("seleccionados")) || []
-console.log(articulosSeleccionados)
+
 fetch (url)
     .then ( response => response.json())
     .then ( res => {
-        productos = res.filter(producto => producto._id)
+        productos = res.filter(producto => producto.disponibles)
+        console.log(productos) 
+        
+        imprimirTarjetasCarrito(articulosSeleccionados, container)
+        //--------------botones-------//
+    //     let contador=0;
+
+    //     const cantidad = document.querySelector('#text');
+    //     const botonMenos = document.getElementById('menos');
+    //     const botonMas = document.getElementById('mas');
+       
+    //    let productoID ;
+        
+        
+    //         const menos = (productoID) =>{
+    //             const producto =productos.find(prod => prod._id=== productoID)
+    //           if (producto && producto.disponibles>0){
+    //               producto.disponibles--;
+    //               actualizar(productoID)
+    //           }
+    //         }  
+    //         const mas = (productoID)=>{
+    //             const producto =productos.find(prod => prod._id=== productoID)
+    //           if (producto){
+    //               producto.disponibles++;
+    //               actualizar(productoID)
+    //           }
+              
+    //         }    
+    //         const actualizar = (productoID) => {
+    //             const producto =productos.find(prod => prod._id=== productoID)
+    //             if (cantidad && producto){
+    //                 cantidad.textContent= producto.disponibles
+    //             }
+    //         }    
+    //         productos.forEach(element => {
+    //             botonMenos.addEventListener('click', menos)    
+    //             botonMas.addEventListener('click', mas)    
+                
+    //         });   
            
-            imprimirTarjetas(articulosSeleccionados, container)
-    })
+    //     })
+    let contador = 0;
+
+    const cantidad = document.querySelector('#text');
+    const botonMenos = document.getElementById('menos');
+    const botonMas = document.getElementById('mas');
+    
+    const menos = () => {
+      if (contador > 0) {
+        contador--;
+        actualizar();
+      }
+    };
+    
+    const mas = () => {
+      contador++;
+      actualizar();
+    };
+    
+    const actualizar = () => {
+      cantidad.textContent = contador;
+    };
+    
+    botonMenos.addEventListener('click', menos);
+    botonMas.addEventListener('click', mas);
+})
+        
     .catch(error => console.log(error))
 
-    const functionEvento = (event) => {
-        const id = event.target.dataset.id
-    
-        if(id){
-            event.target.classList.toggle("btn-danger")
-    
-            const bool = articulosSeleccionados.some(producto => producto._id == id)
-            
-            if(bool){
-                articulosSeleccionados = articulosSeleccionados.filter(producto => producto._id !== id)
-            }
-            else{
-                    const aux = productos.find(producto => producto._id == id)
-            articulosSeleccionados.push(aux)    
-            }
-    
-            localStorage.setItem("seleccionados", JSON.stringify(articulosSeleccionados))
-            console.log(articulosSeleccionados)
-        }
-    }
-    
-    container.addEventListener("click", functionEvento)
+
+      
